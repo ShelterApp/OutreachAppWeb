e84fd5ec78212b57d42e94b3ddd4cb605a71ec03
Tong Tat Hung <tathung095@gmail.com>
2021-11-08 20:34:43 +0700

    edit config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://beta-api.outreachapp.org/:path*',
+      },
+    ]
+  },
+
 }
