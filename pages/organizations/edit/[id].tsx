c44cb269bc397639cd559745fb64555b67bb46ea
duc phan <ducphan2997@gmail.com>
2021-11-17 23:11:38 +0700

    Fixbug

--- pages/organizations/edit/[id].tsx
+++ pages/organizations/edit/[id].tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from "next";
 import styles from "styles/Home.module.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { organizationService } from "services";
 import FormEditOrg from "component/FormEditOrg";
+import { useRouter } from "next/router";
 
-const Edit: NextPage = ({ org }: any) => {
+const Edit: NextPage = () => {
+  const router = useRouter();
+  const [org, setOrg] = useState();
+  const { id } = router.query;
+
+
+  useEffect(() => {
+    const fetch = async () => {
+      const org = await organizationService.get(id);
+      if (org && org._id) {
+        setOrg(org)
+        return;
+      }
+
+      router.push('/404')
+    }
+
+    fetch()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id])
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -15,27 +36,5 @@ const Edit: NextPage = ({ org }: any) => {
     </div>
   );
 };
-
-export async function getStaticPaths() {
-  return {
-    paths: [
-      { params: { id: '*' } }
-    ],
-    fallback: false
-  };
-}
-
-export async function getStaticProps({ params }: any) {
-  const org = await organizationService.get(params.id);
-  if (!(org && org._id)) {
-    return {
-      notFound: true
-    }
-  }
-
-  return {
-    props: { org: org }
-  }
-}
 
 export default Edit;
